ff6ca3023ba6b4e827c107a42273abf2e97a72e6
ellyzyaory <71363310+ellyzyaory@users.noreply.github.com>
2020-09-23 17:47:29 +0700

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+print("A Shinigami [verb] my [adjective] [noun] out of the [noun2] as if he were a vegetarian fishing a [noun3] out of his salad.")
+
+verb = str(input("Enter a verb: "))
+adjective = str(input("Enter an adjective: "))
+noun = str(input("Enter a noun: "))
+noun2 = str(input("Enter a noun: "))
+noun3 = str(input("Enter a noun: "))
+
+
+print("A Shinigami" + " " + verb + " " + "my" + " " + adjective + " " + noun + " " +"out of the" + " " + noun2 + " " + "as if he were a vegetarian fishing a" + " " + noun3 + " " + "out of his salad.")
